61b9eeac55418145e6d0d0ffccf7cbbc4e099dcf
Tomás <troldanborjas@mail.austral.edu.ar>
2024-07-31 08:37:15 -0300

    Completed CollatzConjecture.hs

--- haskell-exercises/collatz-conjecture/src/CollatzConjecture.hs
+++ haskell-exercises/collatz-conjecture/src/CollatzConjecture.hs
@@ -1,4 +1,15 @@
 module CollatzConjecture (collatz) where
 
+--handleEven :: Integer -> Maybe Integer
+--handleEven n
+--  | nextStep == Nothing = Nothing
+--  | otherwise 1 + nextStep
+--    where nextStep = collatz (n `div` 2)
+
 collatz :: Integer -> Maybe Integer
-collatz = error "You need to implement this function."
+collatz n
+  | n <= 0 = Nothing
+  | n == 1 = Just 0
+  | isEven = (+1) <$> collatz (n `div` 2)
+  | otherwise = (+1) <$> collatz (3 * n + 1)
+    where isEven = n `mod` 2 == 0
