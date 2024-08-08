1e7d6102557ac3d8e212703929bb2c40d408574c
Tomás <troldanborjas@mail.austral.edu.ar>
2024-08-07 22:43:27 -0300

    Implemented up to InsertionSort

--- haskell-exercises/lists/src/Lists.hs
+++ haskell-exercises/lists/src/Lists.hs
@@ -1,6 +1,6 @@
 module Lists (member, union, intersection, difference,
               insert, insertionSort,
-              binaryToDecimal, toDecimal, toDec, decimal,
+              binaryToDecimal, toDecimal, toDec, decimal, firsts,
               binaryAdd) where
   
 import Data.Char(digitToInt)  
@@ -19,36 +19,43 @@ union (x:xs) ys
 -- Remove Implementations, from, here on
 
 intersection:: [Int] -> [Int] -> [Int]
-intersection = error "Implement it"
+intersection list1 list2 = [a | a <- list1, member a list2]
+--intersection list1 list2 = fold member
 
 difference:: [Int] -> [Int] -> [Int]
-difference  = error "Implement it"
+difference list1 list2 = [a | a <- list1, not (member a list2)]
 
 insert:: Int -> [Int] -> [Int]
-insert = error "Implement it"
+--insert n list
+--  | n <= (head list) = n : list
+--  | otherwise (head list) : (insert n (tail list))
+insert n [] = n : []
+insert n (x:xs)
+  | n <= x = n : x : xs
+  | otherwise = x : (insert n xs)
 
 insertionSort :: [Int] -> [Int]
-insertionSort = error "Implement it"
+insertionSort list = foldl (flip insert) [] list
 
 binaryToDecimal :: [Int] -> Int
-binaryToDecimal = error "Implement it"
+binaryToDecimal aaaaa = 0
     
 toDecimal :: Int -> [Int] -> Int
-toDecimal = error "Implement it"
+toDecimal aaaaa aaa = 0
     
 toDec::Int -> String -> Int
-toDec base s =  = error "Implement it"
+toDec base s = 0
 
 -- Same as `toDec` But use a list comprehension
 
 decimal::Int -> String -> Int
-decimal  = error "Implement it"
+decimal aaaaa aa  = 0
 
 firsts::[a] -> [[a]]
-firsts = error "Implement it"
+firsts aaa = [[]]
 
 -- Given two String that represents numbers in binary implement the 'binaryAdd' function
 -- DO NOT USE a predefined '+' operation
 
 binaryAdd::String -> String -> String
-binaryAdd  = error "Implement it"
+binaryAdd aa aaaa  = ""
