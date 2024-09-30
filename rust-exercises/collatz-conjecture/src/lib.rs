e8f030caa15ec6759d5d01b5ea22aec0112a26f7
Tomás <troldanborjas@mail.austral.edu.ar>
2024-09-30 16:47:00 -0300

    Completed Collatz

--- rust-exercises/collatz-conjecture/src/lib.rs
+++ rust-exercises/collatz-conjecture/src/lib.rs
@@ -1,4 +1,10 @@
 pub fn collatz(mut n: u64) -> Option<u64> {
-  None
+  collatz_recur(n, 0)
 }
 
+fn collatz_recur(mut n: u64, step: u64) -> Option<u64> {
+  if n < 1           { None }
+  else if n == 1     { Some(step) }
+  else if n % 2 == 0 { collatz_recur(n / 2, step + 1) }
+  else               { collatz_recur(3 * n + 1, step + 1) }
+}
