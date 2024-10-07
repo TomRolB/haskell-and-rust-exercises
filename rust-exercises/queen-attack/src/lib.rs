349f56129f6fc54c4775ce93237e8df402ce360a
Tomás <troldanborjas@mail.austral.edu.ar>
2024-10-07 16:56:58 -0300

    Implemented Queen Attack

--- rust-exercises/queen-attack/src/lib.rs
+++ rust-exercises/queen-attack/src/lib.rs
@@ -8,16 +8,24 @@ pub struct Queen {
 
 impl ChessPosition {
     pub fn new(rank: i32, file: i32) -> Option<Self> {
-        panic!("Implement me");
+        if rank < 0 || rank > 7 || file < 0 || file > 7 { None }
+        else { Some(ChessPosition(rank, file)) }
     }
 }
 
 impl Queen {
     pub fn new(position: ChessPosition) -> Self {
-        panic!("Implement me");
+        Queen { position }
     }
 
     pub fn can_attack(&self, other: &Queen) -> bool {
-    panic!("Implement me");
-}
+        let on_same_rank: bool = self.position.0 == other.position.0;
+        let on_same_file: bool = self.position.1 == other.position.1;
+
+        let rank_delta: i32 = (self.position.0 - other.position.0).abs();
+        let file_delta: i32 = (self.position.1 - other.position.1).abs();
+        let on_diagonal: bool = rank_delta == file_delta;
+
+        on_same_rank | on_same_file | on_diagonal
+    }
 }
