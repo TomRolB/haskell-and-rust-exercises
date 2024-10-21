b47e1b711b5f0f2817fd3d91bcba4e7dffa6602e
Tomás <troldanborjas@mail.austral.edu.ar>
2024-10-21 17:18:44 -0300

    Implemented word count

--- rust-exercises/word-count/src/lib.rs
+++ rust-exercises/word-count/src/lib.rs
@@ -2,5 +2,12 @@ use std::collections::HashMap;
 
 /// Count occurrences of words.
 pub fn word_count(phrase: &str) -> HashMap<String, u32> {
-  panic!("Implement me!")
+  let mut result = HashMap::new();
+
+  phrase
+      .split_whitespace()
+      .map(|x| x.to_lowercase())
+      .for_each(|x| { result.entry(x).and_modify(|y| *y += 1).or_insert(1); });
+
+  result
 }
